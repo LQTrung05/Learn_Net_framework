fc2d2cd2b05b72a991f0c144db07d8928f822626
NamekLQT\NamekTrungLQ <trunglq059@gmail.com>
2025-02-12 17:33:21 +0700

    example about generic class and method

---
+++ Foundational_C#/generic_classes_and_methods/Program.cs
@@ -0,0 +1,32 @@
+﻿
+var list = new GenericClass<string>();
+for(long i = 0; i < 10; i++){
+  list.AddHead(i.ToString());
+}
+foreach (var item in list)
+{
+  Console.WriteLine(item);
+}
+
+public class GenericClass<Q>{
+  private class Node(Q t){
+    public Q Data { get; set; } = t;
+    public Node? Next { get; set; }
+  }
+
+  private Node? head;
+  public void AddHead(Q t){
+    Node n = new(t); //Cách khai báo mới này
+    n.Next = head;
+    head = n;
+  }
+
+  public IEnumerator<Q> GetEnumerator(){
+    Node? curent = head;
+    while(curent is not null){
+      yield return curent.Data;
+      curent = curent.Next;
+    }
+  }
+}
+
