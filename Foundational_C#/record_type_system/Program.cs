2998698b2c7f26a729bcba7c08eef7a1d9a2ac7f
NamekLQT\NamekTrungLQ <trunglq059@gmail.com>
2025-02-12 17:33:59 +0700

    example about record type in C#

---
+++ Foundational_C#/record_type_system/Program.cs
@@ -0,0 +1,15 @@
+﻿public record Person(string FirstName, string LastName, string[] PhoneNumber);
+public static class Program{
+  public static void Main(){
+    var phoneNumbers = new string[2]{ "123456789", "987654321" }; 
+    var person1 = new Person("LQ","Trung", phoneNumbers);
+    var person2 = new Person("LQ","Trung", phoneNumbers);
+    Console.WriteLine(person1 == person2); // output: True
+
+    phoneNumbers[0] = "123456789"; 
+    Console.WriteLine(person1 == person2); // output: True
+    
+    Console.WriteLine(ReferenceEquals(person1, person2)); // output: False
+
+  }
+}
