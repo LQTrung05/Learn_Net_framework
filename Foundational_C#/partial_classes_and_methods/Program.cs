d38572eba8078b12433f02630f1703eaa74bd2c4
NamekLQT\NamekTrungLQ <trunglq059@gmail.com>
2025-02-13 10:50:25 +0700

    example about partial class and methods

---
+++ Foundational_C#/partial_classes_and_methods/Program.cs
@@ -0,0 +1,39 @@
+﻿public partial class Student
+{
+  public int Id { get; set; }
+  public void PrintId()
+  {
+    System.Console.WriteLine("Id: " + Id);
+  }
+  public class NestedClass{
+    public int NestID { get; set; }
+
+  }
+}
+
+public partial class Student
+{
+  public string Name { get; set; }
+  public void PrintName()
+  {
+    Console.WriteLine("Name: " + Name);
+  }
+
+  public Student(int Id, string Name){
+    this.Id = Id;
+    this.Name = Name;
+  }
+}
+public class Program
+{
+  public static void Main()
+  {
+    var student1 = new Student(1, "TrungLQ");
+    student1.PrintId();
+    student1.PrintName();
+
+    var nestObj = new Student.NestedClass();
+    
+  }
+
+}
